e2e41987d6fab7a6550d8b7e2005cd4ae78d6d9e
bex666 <84807059+bex666@users.noreply.github.com>
2025-04-22 20:21:48 +0200

    Add files via upload

--- interface/cli.py
+++ interface/cli.py
@@ -18,7 +18,7 @@ STATIC_MODULES = [
     ('Recherche mobile', 'recherche_mobile'),
     ('Recherche planifiée', 'recherche_planifiee'),
     ('Choix de la ville', 'location'),
-    ('Configuration navigateur', 'browser_config'),
+    ('Configuration du navigateur + vérification de la localisation', 'browser_config'),
     ('Éditer mots-clés', 'edit_keywords'),
     ('IP', 'ip_info'),
     ('Module en test', 'module_test'),
